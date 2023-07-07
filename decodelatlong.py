6cc0a16bfdd3970474936ad7e8984247b74dfb1f
tapankmr72 <134665943+tapankmr72@users.noreply.github.com>
2023-07-07 15:17:43 +0530

    Update decodelatlong.py

--- decodelatlong.py
+++ decodelatlong.py
@@ -1,117 +1,137 @@
 import openpyxl
-path = ""
+path = "C:\\Users\\tapan\\OneDrive\\Desktop\\pythonpremi\\pure python projects\\latlongindia\\"
 wb_obj = openpyxl.load_workbook(path+"latlong.xlsx")
 sheet_obj = wb_obj.active
-srow=2
+print("========================================")
+print("new file")
+a1a=""
+b1=""
+b3=""
+str1c=""
+str1a=""
+a1b=""
+b2=""
+b4=""
+str1d=""
+str1b=""
+print(text)
+pos1=count1.find("<")
+pos1=count1[pos1+1:pos1+2]
+pos2=count1.rfind("<")
+pos2=count1[pos2+1:len(count1)]
+print(pos1)
+print(pos2)
+str1=text[0:int(pos1)]
+str2=text[int(pos1)+1:int(pos2)]
+str3=text[int(pos2)+1:len(text)]
+str1=str1.strip()
+str2=str2.strip()
+str3=str3.strip()
+print(str1)
+print(str2)
+print(str3)
+
+str1a=str1[0:1]
+str1b=str1[1:2]
+str1c=str1[2:3]
+str1d=str1[3:4]
+if str1c!="0" and str1c!="1" and str1c!="2" and str1c!="3" and str1c!="4" and str1c!="5" and str1c!="6" and str1c!="7" and str1c!="8" and str1c!="9":
+    str1c=""
+if str1d!="0" and str1d!="1" and str1d!="2" and str1d!="3" and str1d!="4" and str1d!="5" and str1d!="6" and str1d!="7" and str1d!="8" and str1d!="9":
+    str1d=""
+if str1a!="A" and str1a!="B" and str1a!="C" and str1a!="D" and str1a!="E" and str1a!="F" and str1a!="G" and str1a!="H" and str1a!="I" and str1a!="J":
+    str1a=""
+if str1b!="A" and str1b!="B" and str1b!="C" and str1b!="D" and str1b!="E" and str1b!="F" and str1b!="G" and str1b!="H" and str1b!="I" and str1b!="J":
+    str1a=""
+print(str1a)
+print(str1b)
+print(str1c)
+print(str1d)
+
+if str3[0:1]=="0" or str3[0:1]=="1" or str3[0:1]=="2" or str3[0:1]=="3" or str3[0:1]=="4" or str3[0:1]=="5" or str3[0:1]=="6" or str3[0:1]=="7" or str3[0:1]=="8" or str3[0:1]=="9" :
+ str3a=str3[0:2]
+ next=2
+else:
+ str3a=str3[0:1]
+ next=1
+
+if str3[next:next+1]=="0" or str3[next:next+1]=="1" or str3[next:next+1]=="2" or str3[next:next+1]=="3" or str3[next:next+1]=="4" or str3[next:next+1]=="5" or str3[next:next+1]=="6" or str3[next:next+1]=="7" or str3[next:next+1]=="8" or str3[next:next+1]=="9" :
+ str3b=str3[next:next+2]
+ next=next+2
+else:
+ str3b=str3[next:next+1]
+ next=next+1
+
+if str3[next:next+1]=="0" or str3[next:next+1]=="1" or str3[next:next+1]=="2" or str3[next:next+1]=="3" or str3[next:next+1]=="4" or str3[next:next+1]=="5" or str3[next:next+1]=="6" or str3[next:next+1]=="7" or str3[next:next+1]=="8" or str3[next:next+1]=="9" :
+ str3c=str3[next:next+2]
+ next=next+2
+else:
+ str3c=str3[next:next+1]
+ next=next+1
+if str3[next:next+1]=="0" or str3[next:next+1]=="1" or str3[next:next+1]=="2" or str3[next:next+1]=="3" or str3[next:next+1]=="4" or str3[next:next+1]=="5" or str3[next:next+1]=="6" or str3[next:next+1]=="7" or str3[next:next+1]=="8" or str3[next:next+1]=="9" :
+ str3d=str3[next:next+2]
+ next=next+2
+else:
+ str3d=str3[next:next+1]
+ next=next+1
+
+
+print(str3a)
+print(str3b)
+print(str3c)
+print(str3d)
+
+print(text)
 n=0
-a=""
-b=""
-c=""
-d=""
-e=""
-lat=latid
-long=longid
+srow=2
 while n==0:
 
-    finddotlat=lat.find(".")
-    beforedotlat=lat[0:finddotlat]
-    afterdotlat=lat[finddotlat+1:len(lat)]
+    if str1a=="A":
+        str1a = "0"
 
-    if len(afterdotlat)<2:
-        afterdotlat=afterdotlat+"00000"
-    if len(afterdotlat) < 3:
-        afterdotlat = afterdotlat + "0000"
-    if len(afterdotlat) < 4:
-        afterdotlat = afterdotlat + "000"
-    if len(afterdotlat) < 5:
-        afterdotlat = afterdotlat + "00"
-    if len(afterdotlat) < 6:
-        afterdotlat = afterdotlat + "0"
+    if str1a=="B":
+        str1a = "1"
 
-    #print(beforedotlat)
-    #print(afterdotlat)
-    finddotlong = long.find(".")
-    beforedotlong = long[0:finddotlong]
-    afterdotlong= long[finddotlong + 1:len(long)]
-
-    if len(afterdotlong) < 2:
-        afterdotlong = afterdotlong + "00000"
-    if len(afterdotlong) < 3:
-        afterdotllong = afterdotlong + "0000"
-    if len(afterdotlong) < 4:
-        afterdotlong = afterdotlong + "000"
-    if len(afterdotlong) < 5:
-        afterdotlong = afterdotlong + "00"
-    if len(afterdotlong) < 6:
-        afterdotlong = afterdotlong + "0"
-
-    #print(beforedotlong)
-    #print(afterdotlong)
-
-    str1=beforedotlat+beforedotlong
-    print(str1)
-    str2=afterdotlat[0:2]
-    str3=afterdotlat[2:4]
-    str4a=afterdotlat[4:5]
-    str4b = afterdotlat[5:6]
-    if str4b=="0":
-        str4b = "A"
-
-    if str4b=="1":
-        str4b = "B"
-
-    if str4b=="2":
-        str4b = "C"
-    if str4b=="3":
-        str4b = "D"
-    if str4b=="4":
-        str4b = "E"
-    if str4b=="5":
-        str4b = "F"
-    if str4b=="6":
-        str4b = "G"
-    if str4b=="7":
-        str4b = "H"
-    if str4b=="8":
-        str4b = "I"
-    if str4b=="9":
-        str4b = "J"
+    if str1a=="C":
+        str1a = "2"
+    if str1a=="D":
+        str1a = "3"
+    if str1a=="E":
+        str1a = "4"
+    if str1a=="F":
+        str1a = "5"
+    if str1a=="G":
+        str1a = "6"
+    if str1a=="H":
+        str1a = "7"
+    if str1a=="I":
+        str1a = "8"
+    if str1a=="J":
+        str1a = "9"
 
 
+    if str1b=="A":
+        str1b = "0"
 
-    str5 = afterdotlong[0:2]
-    str6 = afterdotlong[2:4]
-    str7a = afterdotlong[4:5]
-    str7b = afterdotlong[5:6]
-    if str7b == "0":
-        str7b = "A"
+    if str1b=="B":
+        str1b = "1"
 
-    if str7b == "1":
-        str7b = "B"
-
-    if str7b == "2":
-        str7b = "C"
-    if str7b == "3":
-        str7b = "D"
-    if str7b == "4":
-        str7b = "E"
-    if str7b == "5":
-        str7b = "F"
-    if str7b == "6":
-        str7b = "G"
-    if str7b == "7":
-        str7b = "H"
-    if str7b == "8":
-        str7b = "I"
-    if str7b == "9":
-        str7b = "J"
-    lat=beforedotlat+"."+afterdotlat
-    long = beforedotlong + "." + afterdotlong
-    latn = float(lat)
-    longn = float(long)
-    if (int(latn)<8 and int(latn)>38)  or (int(longn)<68 and int(longn)>98):
-        print("You are not Inside Bounadaries of India")
-        continue
+    if str1b=="C":
+        str1b = "2"
+    if str1b=="D":
+        str1b = "3"
+    if str1b=="E":
+        str1b = "4"
+    if str1b=="F":
+        str1b = "5"
+    if str1b=="G":
+        str1b = "6"
+    if str1b=="H":
+        str1b = "7"
+    if str1b=="I":
+        str1b = "8"
+    if str1b=="J":
+        str1b = "9"
     cell_obj1 = sheet_obj.cell(row=srow, column=1)
     cell_obj2 = sheet_obj.cell(row=srow, column=2)
     cell_obj3 = sheet_obj.cell(row=srow, column=3)
@@ -122,40 +142,53 @@ while n==0:
     cell_obj8 = sheet_obj.cell(row=srow, column=8)
     cell_obj9 = sheet_obj.cell(row=srow, column=9)
     cell_obj10 = sheet_obj.cell(row=srow, column=10)
-    #print(str1)
-    #print(str2)
-    #print(str3)
-    #print(str4a)
-    #print(str4b)
-    #print(str5)
-    #print(str6)
-    #print(str7a)
-    #print(str7b)
-    if cell_obj1.value==str1:
-       a= cell_obj2.value
-
-    if cell_obj3.value==str2:
-       b= cell_obj4.value
-    if cell_obj3.value==str5:
-       c= cell_obj4.value
-    if cell_obj3.value==str3:
-       d= cell_obj4.value
-    if cell_obj3.value==str6:
-       e= cell_obj4.value
-
-
     if cell_obj1.value == None or cell_obj1.value == "":
        break
+    print(str2)
+
+    #print("hello :"+cell_obj2.value.upper())
+    if cell_obj2.value.strip().upper()==str2.strip():
+       a1= cell_obj1.value.strip()
+       if len(a1)==3:
+        a1a=a1[0:1]
+        a1b=a1[1:3]
+       else:
+           a1a = a1[0:2]
+           a1b = a1[2:4]
+
+    print("a1a: "+a1a)
+    if cell_obj4.value==str3a:
+       b1= cell_obj3.value
+    if cell_obj4.value == str3b:
+        b2 = cell_obj3.value
+    if cell_obj4.value == str3c:
+        b3 = cell_obj3.value
+    if cell_obj4.value == str3d:
+        b4 = cell_obj3.value
+
+
+
 
     srow=srow+1
+print("------------------------------------------")
+print(a1a)
+print(a1b)
+print(b1)
+print(b2)
+print(b3)
+print(b4)
+print(str1a)
+print(str1b)
+print(str1c)
+print(str1d)
 
 #print(a)
-if a!="":
-    address = str4b+str7b+str4a+str7a + "." + (a.strip() + "." + b.strip() +  c.strip() + d.strip() + e.strip())
+if a1a!="" and a1b!="" and b1!="" and b2!="" and b3!="" and b4!=""  and str1a!="" and str1b!="" and str1c!="" and str1d!="":
+    lat = a1a+"."+b1+b3+str1c+str1a
+    long=a1b+"."+b2+b4+str1d+str1b
+    message = "https://www.google.com/maps/search/?api=1&query=" + lat + "," + long
 else:
- address=""
-
-print(address)
+  message="?"
 
 
 
