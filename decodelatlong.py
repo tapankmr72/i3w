7c506880f7e7e78da10ad62017727d4513585ceb
tapankmr72 <134665943+tapankmr72@users.noreply.github.com>
2023-07-11 09:18:46 +0530

    Update decodelatlong.py

--- decodelatlong.py
+++ decodelatlong.py
@@ -13,7 +13,7 @@ while n == 0:
 
         break
 
-    if cell_obj2a.value.upper().strip() == text.upper().strip() :
+    if cell_obj2a.value.strip().lower() == text.strip().lower() :
         text=cell_obj1a.value
         strLen = len(text)
         range_obj = range(strLen)
@@ -216,6 +216,7 @@ if a1a!="" and a1b!="" and b1!="" and b2!="" and b3!="" and b4!=""  and str1a!="
     message = "https://www.google.com/maps/search/?api=1&query=" + lat + "," + long
 else:
   message="?"
+  text="  
 
 
 
